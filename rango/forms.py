2c957bddca9bd6f36d6116a37fde90b87f0d87b4
bharu1393 <rajatbhardwaj@Rajats-MacBook-Pro.local>
2022-02-11 11:14:32 +0000

    Chapter 9 code

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms 
 from rango.models import Page,Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.MAX_NAME_VALUE,help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -27,6 +29,17 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
             return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta: 
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
 
 
 
--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     MAX_NAME_VALUE = 128
@@ -24,3 +24,9 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     
     def __str__(self): return self.title
+
+class UserProfile(models.Model):
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
